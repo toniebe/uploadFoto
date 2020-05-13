88ede87ad1b8430b2ddd9021d5c223be8c787b17
toniebe <toniebe48@gmail.com>
2020-05-13 23:53:36 +0700

    dslkfjaldsk

--- src/main/resources/db.migration/V2020130501__Skema_Awal.sql
+++
@@ -1,9 +0,0 @@
-create table transaksi {
-    idtransaksi int(90) SERIAL PRIMARY KEY,
-    username varchar (100),
-    nomer varchar (100),
-    provider varchar (100),
-    paket varchar (100),
-    harga float ,
-    tanggal date
-};
